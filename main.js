f22630e7ba544982d63d59439ed9f8c986e16c1f
Yogesh Poonia <yogidarapoonia@gmail.com>
2023-04-07 20:56:26 +0530

    added vibrate on click

--- main.js
+++ main.js
@@ -95,6 +95,7 @@ function check(user, comp) {
 }
 
 function handle(str) {
+    navigator.vibrate("100ms");
     let x = Math.random();
     check(str, x < 0.33 ? "rock" : (x < 0.66 ? "paper" : "scissors"))
 }
