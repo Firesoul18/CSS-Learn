9a5e63c631621c57edb450dac784f4c495e5935c
Yogesh Poonia <yogidarapoonia@gmail.com>
2023-04-07 21:09:04 +0530

    changed vibrate time

--- main.js
+++ main.js
@@ -95,7 +95,7 @@ function check(user, comp) {
 }
 
 function handle(str) {
-    navigator.vibrate(900);
+    navigator.vibrate(150);
     let x = Math.random();
     check(str, x < 0.33 ? "rock" : (x < 0.66 ? "paper" : "scissors"))
 }
